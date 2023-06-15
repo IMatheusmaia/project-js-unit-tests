e732e9f4f516f28fc800cca0dc6191db02b57769
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-06-15 17:24:19 -0300

    requisito 10 testado até o item 8

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,29 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = () => {};
+const createMenu = (comidas, bebidas) => {
+    const objectFD = { food: Object(comidas), 
+        drinks: Object(bebidas),
+    };
+    const menu = { 
+        fetchMenu: () => objectFD,
+        consumption: [],
+        order: (pedido) => {
+            const foodKeys = Object.keys(objectFD.food);
+            const drinksKeys = Object.keys(objectFD.drinks);
+            const keysObjFD = foodKeys.concat(drinksKeys);
+            
+           const find = keysObjFD.find((element) => element === pedido);
+           if (typeof find === 'string') { 
+            menu.consumption.push(find);
+
+            return menu.consumption;
+        } 
+            return 'Item indisponível';
+        },
+    };
+    return menu;
+};
+console.log(createMenu({ coxinha: 3.90, sanduiche: 9.90 }, { agua: 3.90, cerveja: 6.90 }).order('coxinha'));
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,8 +1,21 @@
 const createMenu = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
-  it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // Escreva todos os testes aqui.
+  it('Verifica se a função `createMenu` retorna um objeto com a chave fetchMenu e se essa propriedade é uma função', () => {
+    expect(createMenu()).toHaveProperty('fetchMenu');
+    expect(typeof createMenu()['fetchMenu']).toBe('function');
+  });
+  it('Verifica se o objeto retornado pela chave fetchMenu possui apenas duas propriedades food e drinks respectivamente', () => {
+    expect(Object.keys(createMenu().fetchMenu())).toEqual(['food', 'drinks']);
+  });
+  it('Verifica se o menu passado como parâmetro na função é recuperado na saída da função createMenu().fetchMenu()', () => {
+    expect(createMenu({ coxinha: 3.9 }, { agua: 3.9 } ).fetchMenu()).toEqual({ food: { coxinha: 3.9 }, drinks: { agua: 3.9 } })
+  });
+  it('Verifica se ao chamar a função createMenu, a propriedade consumption do objeto de retorno tem o valor de um array vazio', () => {
+    expect(createMenu()).toHaveProperty('consumption',[]);
+  });
+  it('Verificar se a mensagem de "item indisponível" é retornada quando o parâmetro passado na função ordem não corresponde a nenhuma propriedade de objeto e retorna corretamente o item quando existe a propriedade', () => {
+    expect(createMenu({ coxinha: 3.90, sanduiche: 9.90 }, { agua: 3.90, cerveja: 6.90 }).order('empada')).toBe('Item indisponível');
+    expect(createMenu({ coxinha: 3.90, sanduiche: 9.90 }, { agua: 3.90, cerveja: 6.90 }).order('cerveja')).toEqual(['cerveja']);
   });
 });
